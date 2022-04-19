46a4ff6e4d738ca48ca8ab7b681211aaf947a267
mehmettkutann <mehmettkutann>
2022-04-19 16:39:20 +0300

    Message

---
+++ Mehmet/arm_test.py
@@ -0,0 +1,122 @@
+from tokenize import String
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative, Command
+from numpy import take
+from pymavlink import mavutil  # Needed for command message definitions
+import time
+import math
+
+from sympy import false, true
+from tomlkit import boolean
+#--------------------------------------------------------->
+vehicle=connect('/dev/ttyUSB0', baud=57600)
+vehicle.mode = VehicleMode("STABILIZE")
+print("Mode:",vehicle.mode)
+#--------------------------------------------------------->
+irtifa_degeri=1
+inis_degeri=0.4
+def set_velocity_body(vehicle, vx, vy, vz):
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+    0,
+    0, 0,
+    mavutil.mavlink.MAV_FRAME_BODY_NED,
+    0b0000111111000111,  # -- BITMASK -> Consider only the velocities
+    0, 0, 0,  # -- POSITION
+    vx, vy, vz,  # -- VELOCITY
+    0, 0, 0,  # -- ACCELERATIONS
+    0, 0)
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+
+def arm():
+    try:
+        while not vehicle.armed:
+            print( "Waiting for vehicle to initialise...")
+            vehicle.arm()
+            time.sleep(1)
+        while  vehicle.armed:
+            print(vehicle.mode)
+            print( "Arming motor")
+            time.sleep(5)
+            battery_check()
+            kontrol_degeri=takeoff(irtifa_degeri)
+            print("kontrol degeri--->",kontrol_degeri)
+            if kontrol_degeri>=irtifa_degeri:
+                print("you are aTargetAltitude")
+                print("Havada bekliyorum")
+                time.sleep(5)
+                land()
+                
+    except KeyboardInterrupt:
+        print('arm-stopped')
+        vehicle.mode = VehicleMode("LAND")
+        print(vehicle.mode)
+        vehicle.disarm()
+        print("disarming")
+
+def temp_land():
+    print("Vehicle in LAND mode")
+    vehicle.mode = VehicleMode("LAND")
+    while not vehicle.location.global_relative_frame.alt==0:
+        if vehicle.location.global_relative_frame.alt < 2:
+            set_velocity_body(vehicle,0,0,0.1)
+        print ("Vehicle in AUTO mode")
+    vehicle.mode = VehicleMode("AUTO")
+def land():
+    print("Vehicle in LAND mode")
+    vehicle.mode = VehicleMode("LAND")
+    while not vehicle.location.global_relative_frame.alt==0:
+        print("land->mode",vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt < 2:
+            set_velocity_body(vehicle,0,0,0.1)
+    #vehicle.armed = False
+    #vehicle.close()
+def about_battery():
+    print("*******************************")
+    print ("About Battery: %s" % vehicle.battery)
+    batterycheck=int(vehicle.battery.level)
+    if batterycheck <= 25:
+        print("Battery is loww!!! not started")
+        vehicle.disarm()
+        print("disarming")
+    print("*****************************")
+def battery_check():
+    batterycheck=float(vehicle.battery.voltage)
+    if batterycheck <= 12.0:
+        print("Battery is loww!!! go home")
+    else:
+        print ("Battery: %s" % vehicle.battery.level)
+
+def takeoff(aTargetAltitude):
+    try:
+        vehicle.mode = VehicleMode("GUIDED")
+        print(vehicle.mode)
+        vehicle.armed = True
+        vehicle.simple_takeoff(aTargetAltitude)
+        time.sleep(5)
+        while True:
+                print(vehicle.location.global_relative_frame.alt,"---->",aTargetAltitude * 0.95)
+                if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+                    print("Reached target altitude")
+                    if aTargetAltitude * 0.95>0:
+                        time.sleep(3)
+                        return aTargetAltitude
+                time.sleep(1)     
+    except KeyboardInterrupt:
+        print('takeoff-stopped')
+        vehicle.mode = VehicleMode("LAND")
+        print(vehicle.mode)
+        vehicle.disarm()
+############# POINTS ###############
+
+print (" Type: %s" % vehicle._vehicle_type)
+print (" System status: %s" % vehicle.system_status.state)
+print (" GPS: %s" % vehicle.gps_0)
+print (" Alt: %s" % vehicle.location.global_relative_frame.alt)
+if vehicle.location.global_relative_frame.alt is None:
+    print("GPS is None")
+elif vehicle.location.global_relative_frame.alt<0:
+    print("GPS<0")
+else: 
+    home = vehicle.location.global_relative_frame
+    about_battery()  
+    arm()
